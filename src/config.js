41391d048ffc3a052abe5d99cecc204aa7c5d32b
kakashi 404 <72741431+ahmadgovan@users.noreply.github.com>
2024-09-07 17:09:38 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    token: "",
-    prefix: "",
-    ownerID: "",
+    token: "MTI4MTM0NjE3NTg2MDk5ODE3NA.G2Srhv.zfny-PTQCrT8iGtizhScu3QHtfvnytwHH2SjlY",
+    prefix: "-",
+    ownerID: "1244674806075490365",
     SpotifyID: "",
     SpotifySecret: "",
-    mongourl: "",
-    embedColor: "",
-    logs: "",
+    mongourl: "mongodb+srv://music:<db_password>@cluster0.f1oio.mongodb.net/",
+    embedColor: "RANDOM",
+    logs: "1281346175860998174", //Log channel join and left
 
   nodes: {
      
